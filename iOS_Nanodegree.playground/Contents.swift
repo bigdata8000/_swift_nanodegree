d3f7688ce6ac7b0a8294cee300472e76eceefcb4
Henry Phu-Henh Lam <50170584+henrymmmm@users.noreply.github.com>
2020-07-13 23:01:09 +0100

    Add files via upload

---
+++ iOS_Nanodegree.playground/Contents.swift
@@ -0,0 +1,61 @@
+// Udacity - iOS Developer Nannodegree
+// Part 2 - lesson 01
+
+import UIKit
+
+var question = "Ready to write your first lines of Swift?"
+var response = "Heck yeah, I'm ready!"
+
+print(question)
+print("Hello, world!")
+
+
+// Declaring Variables of Different Types
+
+var numberOfWheels: Int = 4
+var centimetersOfRainfall: Float = 5.5
+var pi: Double = 3.14159265359
+var letterOfTheDay: Character = "z"
+var myFavoriteAnimal: String  = "octopus"
+var rainingOutside: Bool  = true
+
+
+// 12 - Constants = let (value can't change)
+
+let birthYear = 2008                // this value won't change!
+var currentYear = 2020               // this value will change...
+let age = currentYear - birthYear
+print("You are \(age) years old.")
+
+
+// Tips With String Literals
+
+let emailSignature = "Aliyah Smith\nVP, Sales\n(555) - 555 - 5555"
+print(emailSignature)
+
+/*
+
+ Character    Usage
+ \n    Newline / line feed
+ \t    Horizontal tab
+ \"    Double quotation mark, using " alone signifies the end of a string
+ \'    Single quotation mark
+ \    Backslash, using "\" alone escapes the next character
+ 
+ */
+
+print("Hello \n World")
+
+
+// Part 2 - Lesson 02: Operators and Expressions
+
+/*
+ Ternary Operators
+ 
+ trueOrFalseValue ? useThisIfTrue : useThisIfFalse
+ 
+*/
+
+
+// Part 2 - Lesson 04:  Working With Operator Expressions
+
